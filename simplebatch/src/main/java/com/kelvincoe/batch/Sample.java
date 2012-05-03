a2ac5643cf5c81b6dd6fc09ef29f89598df77326
Selvan_Kumar <selvan52@gmail.com>
2012-05-03 17:22:46 +0530

    newly added pojo class

---
+++ simplebatch/src/main/java/com/kelvincoe/batch/Sample.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.kelvincoe.batch;
+
+/**
+ * @author admin
+ * 
+ */
+public class Sample {
+	private String name;
+	private String age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+}
